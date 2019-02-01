f4a2fcd74b13ec76468044be0fd92a49aa365220
zaqueu rodrigues <zakeurodrigues@outlook.com>
2019-02-01 08:55:45 -0200

    Classe Posicao

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,13 @@
 package aplication;
 
+import Jogo.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
-
+		
+		Posicao p = new Posicao(3, 5);
+		System.out.println(p);
 	}
 
 }
